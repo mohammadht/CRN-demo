e637d6e786e13dfc12bc6b1ec01f22e794940902
mohammadht <mohammht@ifi.uio.no>
2022-07-21 10:55:09 +0000

    test

--- smartMed/pyprocessor/cookiejar_tp.py
+++ smartMed/pyprocessor/cookiejar_tp.py
@@ -186,7 +186,7 @@ class CookieJarTransactionHandler(TransactionHandler):
         for line in lines:
             data = line.split(",")
             fw.write(data[2])
-            if data[2].__eq__(amount):
+            if amount == "green":
                 fw.write(data[1])
                 fw.write("\n")
         fr.close()
