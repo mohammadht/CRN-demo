c0c25fece6b8d0ac3424141df5709d07bb1a72e4
mohammadht <mohammht@ifi.uio.no>
2022-07-21 13:19:42 +0000

    amount

--- smartMed/pyprocessor/cookiejar_tp.py
+++ smartMed/pyprocessor/cookiejar_tp.py
@@ -183,10 +183,14 @@ class CookieJarTransactionHandler(TransactionHandler):
         fr = open("./pyprocessor/dslist.txt","r")
         fw = open("./pyprocessor/ds-color.txt","w")
         lines = fr.readlines()
+        fw.write(amount)
+        fw.write("\n")
         for line in lines:
             line.strip()
             data = line.split(",")
-            if str(data[2]) == amount:
+            fw.write(data[2])
+            fw.write("\n")
+            if data[2] == amount:
                 fw.write(data[1])
                 fw.write("\n")
         fr.close()
