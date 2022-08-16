bbe1391ed96ef680b9f68327400730935a2af640
mohammadht <mohammht@ifi.uio.no>
2022-08-16 07:20:52 +0000

    rename files

---
+++ smartMed/pyclient/smartmed.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+'''
+Command line interface for cookiejar TF.
+Parses command line arguments and passes to the CookieJarClient class
+to process.
+'''
+
+import argparse
+import logging
+import os
+import sys
+import traceback
+
+from colorlog import ColoredFormatter
+from cookiejar_client import CookieJarClient
+
+KEY_NAME = 'mycookiejar'
+
+# hard-coded for simplicity (otherwise get the URL from the args in main):
+DEFAULT_URL = 'http://localhost:8008'
+# For Docker:
+# DEFAULT_URL = 'http://rest-api:8008'
+
+def create_console_handler(verbose_level):
+    '''Setup console logging.'''
+    del verbose_level # unused
+    clog = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        "%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s "
+        "%(white)s%(message)s",
+        datefmt="%H:%M:%S",
+        reset=True,
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red',
+        })
+
+    clog.setFormatter(formatter)
+    clog.setLevel(logging.DEBUG)
+    return clog
+
+def setup_loggers(verbose_level):
+    '''Setup logging.'''
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(create_console_handler(verbose_level))
+
+def create_parser(prog_name):
+    '''Create the command line argument parser for the cookiejar CLI.'''
+    parent_parser = argparse.ArgumentParser(prog=prog_name, add_help=False)
+
+    parser = argparse.ArgumentParser(
+        description='Provides subcommands to manage your simple cookie baker',
+        parents=[parent_parser])
+
+    subparsers = parser.add_subparsers(title='subcommands', dest='command')
+    subparsers.required = True
+
+    find_subparser = subparsers.add_parser('find',
+                                           help='find the list of DSs with color tag',
+                                           parents=[parent_parser])                                           
+    find_subparser.add_argument('color',
+                                type=str,
+                                help='the color to be found')
+    find_subparser.add_argument('--qid',
+                               type=int,
+                               help='query id of the request')
+    list_subparser = subparsers.add_parser('list',
+                                           help='display all of the query results',
+                                           parents=[parent_parser])
+    interested_subparser = subparsers.add_parser('interested',
+                                           help='The DS shows its interest to the query',
+                                           parents=[parent_parser])
+    interested_subparser.add_argument('--dsid',
+                                type=str,
+                                help='the ID of the DS that is going to answer the query')
+    interested_subparser.add_argument('--qid',
+                                type=int,
+                                help='the ID of the query needed to be answered')
+    interested_subparser.add_argument('status',
+                                type=str,
+                                help='YES/NO response from the DS')
+    delete_subparser = subparsers.add_parser('delete',
+                                          help='delete a registered query',
+                                          parents=[parent_parser])
+    delete_subparser.add_argument('qid',
+                               type=int,
+                               help='Query ID of the one that is going to be deleted')                                                                                                                                                                                                                                      			  
+    return parser
+
+def _get_private_keyfile(key_name):
+    '''Get the private key for key_name.'''
+    home = os.path.expanduser("~")
+    key_dir = os.path.join(home, ".sawtooth", "keys")
+    return '{}/{}.priv'.format(key_dir, key_name)
+
+def do_find(args):
+    '''Subcommand to find a list of DSs with associated color. Calls client class to do the finding.'''
+    privkeyfile = _get_private_keyfile(KEY_NAME)
+    client = CookieJarClient(base_url=DEFAULT_URL, key_file=privkeyfile)
+    response = client.find(args.color,args.qid)
+    print("Find Response: {}".format(response))
+
+def do_interested(args):
+    '''Subcommand to show the interest of the DS to a query. Calls client class to do the interest.'''
+    privkeyfile = _get_private_keyfile(KEY_NAME)
+    client = CookieJarClient(base_url=DEFAULT_URL, key_file=privkeyfile)
+    data = client.get_query(args.qid)
+    if data is not None:
+        qid, ds1, ds2, ds3, ds4, ds5 = data.decode().split(",")
+    response = client.interested(args.dsid,args.qid,args.status,ds1,ds2,ds3,ds4,ds5)
+    print("Find Response: {}".format(response))    
+
+def do_list():
+    '''Subcommand to show the list of query results.  Calls client class to do the showing.'''
+    privkeyfile = _get_private_keyfile(KEY_NAME)
+    client = CookieJarClient(base_url=DEFAULT_URL, key_file=privkeyfile)
+    query_list = [
+        tx.split(',')
+        for txs in client.list()
+        for tx in txs.decode().split('|')
+    ]
+    if query_list is not None:
+        count = 0;
+        for tx_data in query_list:
+            count = count + 1;
+            qid, ds1, ds2, ds3, ds4, ds5 = tx_data
+            print(str(count) + ") Query ID:"+ qid, \
+                "| DS1:"+ ds1, \
+                "| DS2:"+ ds2, \
+                "| DS3:"+ ds3, \
+                "| DS4:"+ ds4, \
+                "| DS5:"+ ds5)
+    else:
+        raise Exception("Transaction data not found")            
+
+def do_delete(args):
+    '''Subcommand to delete a query.  Calls client class to do the deleting.'''
+    privkeyfile = _get_private_keyfile(KEY_NAME)
+    client = CookieJarClient(base_url=DEFAULT_URL, key_file=privkeyfile)
+    response = client.delete(args.qid)
+    print("delete Response: {}".format(response))    
+
+
+def main(prog_name=os.path.basename(sys.argv[0]), args=None):
+    '''Entry point function for the client CLI.'''
+    try:
+        if args is None:
+            args = sys.argv[1:]
+        parser = create_parser(prog_name)
+        args = parser.parse_args(args)
+        verbose_level = 0
+        setup_loggers(verbose_level=verbose_level)
+
+        # Get the commands from cli args and call corresponding handlers
+        if args.command == 'find':
+            do_find(args)
+        elif args.command == 'interested':
+            do_interested(args)
+        elif args.command == 'delete':
+            do_delete(args)        
+        elif args.command == 'list':
+            do_list()                   	
+        else:
+            raise Exception("Invalid command: {}".format(args.command))
+
+    except KeyboardInterrupt:
+        pass
+    except SystemExit as err:
+        raise err
+    except BaseException as err:
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
---
+++ smartMed/pyprocessor/smartmed_tp.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+'''
+CookieJarTransactionHandler class interfaces for cookiejar Transaction Family.
+'''
+
+import traceback
+import sys
+import hashlib
+import logging
+import random
+import string
+import os.path
+
+from sawtooth_sdk.processor.handler import TransactionHandler
+from sawtooth_sdk.processor.exceptions import InvalidTransaction
+from sawtooth_sdk.processor.exceptions import InternalError
+from sawtooth_sdk.processor.core import TransactionProcessor
+
+# hard-coded for simplicity (otherwise get the URL from the args in main):
+DEFAULT_URL = 'tcp://localhost:4004'
+# For Docker:
+#DEFAULT_URL = 'tcp://validator:4004'
+
+LOGGER = logging.getLogger(__name__)
+
+FAMILY_NAME = "cookiejar"
+# TF Prefix is first 6 characters of SHA-512("cookiejar"), a4d219
+
+def _hash(data):
+    '''Compute the SHA-512 hash and return the result as hex characters.'''
+    return hashlib.sha512(data).hexdigest()
+
+def _get_cookiejar_address(from_key,query):
+    '''
+    Return the address of a cookiejar object from the cookiejar TF.
+
+    The address is the first 6 hex characters from the hash SHA-512(TF name),
+    plus the result of the hash SHA-512(cookiejar public key).
+    '''
+    return _hash(FAMILY_NAME.encode('utf-8'))[0:6] + \
+                 _hash(from_key.encode('utf-8'))[0:32] + _hash(query.encode('utf-8'))[0:32]
+
+
+class CookieJarTransactionHandler(TransactionHandler):
+    '''
+    Transaction Processor class for the cookiejar Transaction Family.
+
+    This TP communicates with the Validator using the accept/get/set functions.
+    This implements functions to "find", "bake" or "eat" cookies in a cookie jar.
+    '''
+    def __init__(self, namespace_prefix):
+        '''Initialize the transaction handler class.
+
+           This is setting the "cookiejar" TF namespace prefix.
+        '''
+        self._namespace_prefix = namespace_prefix
+
+    @property
+    def family_name(self):
+        '''Return Transaction Family name string.'''
+        return FAMILY_NAME
+
+    @property
+    def family_versions(self):
+        '''Return Transaction Family version string.'''
+        return ['1.0']
+
+    @property
+    def namespaces(self):
+        '''Return Transaction Family namespace 6-character prefix.'''
+        return [self._namespace_prefix]
+
+    def apply(self, transaction, context):
+        '''This implements the apply function for the TransactionHandler class.
+
+           The apply function does most of the work for this class by
+           processing a transaction for the cookiejar transaction family.
+        '''
+
+        # Get the payload and extract the cookiejar-specific information.
+        # It has already been converted from Base64, but needs deserializing.
+        # It was serialized with CSV: action, value
+        header = transaction.header
+        payload_list = transaction.payload.decode().split(",")
+        action = payload_list[0]
+        if action == "find":
+            amount = payload_list[1]
+            qid = payload_list[2]
+        elif action == "interested":
+            amount = payload_list[1]
+            qid = payload_list[2]
+            status = payload_list[3]
+            ds1 = payload_list[4]
+            ds2 = payload_list[5]
+            ds3 = payload_list[6]
+            ds4 = payload_list[7]
+            ds5 = payload_list[8]
+        elif action == "delete":
+            qid = payload_list[1]
+
+        # Get the signer's public key, sent in the header from the client.
+        from_key = header.signer_public_key
+
+        # Perform the action.
+        if action == "find":
+            LOGGER.info("Amount = %s.", amount)   
+            LOGGER.info("Query ID = %s.", qid)
+            self._make_find(context, amount, qid, from_key)
+        elif action == "interested":
+            LOGGER.info("Amount = %s.", amount)        
+            LOGGER.info("Query ID = %s.", qid)
+            LOGGER.info("status = %s.", status)
+            LOGGER.info("ds1 = %s.", ds1)
+            LOGGER.info("ds2 = %s.", ds2)
+            LOGGER.info("ds3 = %s.", ds3)
+            LOGGER.info("ds4 = %s.", ds4)
+            LOGGER.info("ds5 = %s.", ds5)
+            self._make_interested(context, amount, qid, status, ds1, ds2, ds3, ds4, ds5, from_key)            
+        elif action == "delete":
+            LOGGER.info("Query ID = %s.", qid)
+            self._make_delete(context, qid, from_key)
+        else:
+            LOGGER.info("Unhandled action. Action should be bake or eat")
+
+    @classmethod
+    def _make_find(cls, context, amount, qid, from_key):
+        '''find associated dsc from a specific dc based on the color tag.'''
+        query_address = _get_cookiejar_address(from_key,qid)
+        LOGGER.info('Got the key %s and the query address %s.',
+                    from_key, query_address)
+        query_result = [qid,"n/a","n/a","n/a","n/a","n/a"]
+        fr = open("./pyprocessor/dslist.txt","r")
+        fw = open("./pyprocessor/ds-color.txt","w")
+        lines = fr.readlines()
+        for line in lines:
+            data = line.strip().split(",")
+            if data[2].casefold() == amount:
+                if data[1] == "DS1Pubkey":
+                    query_result[1] = "waiting"                   
+                if data[1] == "DS2Pubkey":
+                    query_result[2] = "waiting"
+                if data[1] == "DS3Pubkey":
+                    query_result[3] = "waiting"
+                if data[1] == "DS4Pubkey":
+                    query_result[4] = "waiting"
+                if data[1] == "DS5Pubkey":
+                    query_result[5] = "waiting"            
+        fw.write(data[1])
+        fw.write("\n")               
+        fr.close()
+        fw.close()
+        state_data = str(query_result).encode('utf-8')
+        addresses = context.set_state({query_address: state_data})
+
+    def _make_interested(cls, context, amount, qid, status, ds1, ds2, ds3, ds4, ds5, from_key):
+        '''Register the interest of a DS to a query.'''
+        query_address = _get_cookiejar_address(from_key,qid)
+        LOGGER.info('Got the key %s and the cookiejar address %s.',
+                    from_key, query_address)
+        state_entries = context.get_state([query_address])
+        qid, ds1, ds2, ds3, ds4, ds5 = state_entries[0].data.decode().split(',')        
+        if status == "yes":
+            status = "inetersted"
+        else:
+            status = "not interested"    
+        if amount == "ds1":
+            query_result = qid, status, ds2, ds3, ds4, ds5
+        if amount == "ds2":
+            query_result = qid, ds1, status, ds3, ds4, ds5
+        if amount == "ds3":
+            query_result = qid, ds1, ds2, status, ds4, ds5
+        if amount == "ds4":
+            query_result = qid, ds1, ds2, ds3, status, ds5
+        if amount == "ds5":
+            query_result = qid, ds1, ds2, ds3, ds4, status                
+        state_data = str(query_result).encode('utf-8')
+        addresses = context.set_state({query_address: state_data})
+
+        if len(addresses) < 1:
+            raise InternalError("State Error")
+        context.add_event(
+            event_type="cookiejar/bake",
+            attributes=[("cookies-baked", amount)])    
+
+    @classmethod
+    def _make_delete(cls, context, qid, from_key):
+        query_address = _get_cookiejar_address(from_key,qid)
+        LOGGER.info('Got the key %s and the query address %s.',
+                    from_key, query_address)
+        context.delete_state([query_address])    
+
+def main():
+    '''Entry-point function for the cookiejar Transaction Processor.'''
+    try:
+        # Setup logging for this class.
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+
+        # Register the Transaction Handler and start it.
+        processor = TransactionProcessor(url=DEFAULT_URL)
+        sw_namespace = _hash(FAMILY_NAME.encode('utf-8'))[0:6]
+        handler = CookieJarTransactionHandler(sw_namespace)
+        processor.add_handler(handler)
+        processor.start()
+    except KeyboardInterrupt:
+        pass
+    except SystemExit as err:
+        raise err
+    except BaseException as err:
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
